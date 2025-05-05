cbf62232947c54913f9e3c3e1cb31aaa81b0ad31
jmherrera <jerreramarcia@gmail.com>
2025-05-05 15:43:05 +0100

    Clean up

--- src/main/java/com/alic3/versioned/jwt/JwtFilter.java
+++ src/main/java/com/alic3/versioned/jwt/JwtFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,12 +16,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@AllArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
-    @Autowired
     private JwtUtil jwtUtil;
 
-    @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
